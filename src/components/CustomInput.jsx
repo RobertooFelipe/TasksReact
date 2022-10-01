85465aacec10cd2516b180be20c746ec8e757b0a
RobertooFelipe <feliperoberto092@gmail.com>
2022-10-01 00:45:36 -0300

    fix: CustomInput label

--- src/components/CustomInput.jsx
+++ src/components/CustomInput.jsx
@@ -1,13 +1,18 @@
-const CustomInput = ({ label, value }) => {
+import "./CustomInput.scss"
+
+const CustomInput = ({ label, value, onChange }) => {
     return (
         <div className="custom-input-container">
-        <input type="text" className="custom-input" />
+        <input 
+            type="text" 
+            className="custom-input"
+            onChange={(e) => onChange(e)} />
 
         {label ? (
             <label className={`${
-                value.lenght !== 0 ? "shrink" : ""
+                value.lenght > 0 ? "shrink" : ""
             } custom-input-label`}
-            ></label>
+            >{label}</label>
         ) : null}
     </div>
     )
