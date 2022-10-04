c935e0a97911619bae13d89ef7ef37518e59494e
RobertooFelipe <feliperoberto092@gmail.com>
2022-10-04 08:12:13 -0300

    fix: add retun to CutomButtom component

--- src/components/CustomButton/CustomButton.jsx
+++ src/components/CustomButton/CustomButton.jsx
@@ -1,9 +1,11 @@
 import './CustomButton.scss'
 
 const CustomButton = ({ children, onClick }) => {
-    <div className="custom-button-container" onClick={onClick}>
-        {children}
-    </div>
+    return(
+        <div className="custom-button-container" onClick={onClick}>
+            {children}
+        </div>
+    )
 }
 
 export default CustomButton;
